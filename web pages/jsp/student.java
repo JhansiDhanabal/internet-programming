ebcd148fe8ad11b5190550f1a0ce834e918fc189
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2023-12-25 22:13:43 +0530

    Create student.java

---
+++ web pages/jsp/student.java
@@ -0,0 +1,11 @@
+package jstl;
+public class student {
+    public String name;
+    public int grade;
+    public String email;
+    public student(String name,int grade,String email){
+        this.name=name;
+        this.grade=grade;
+        this.email=email;
+    }
+}
